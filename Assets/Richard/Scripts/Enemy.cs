66b6a711cce48227795e0de8fd05158017337fb7
GustyWinds24 <tgustis@hotmail.com>
2018-11-02 23:00:03 -0500

    Added animations to wolves.
    Added animations to wolves.

--- Assets/Richard/Scripts/Enemy.cs
+++ Assets/Richard/Scripts/Enemy.cs
@@ -9,47 +9,13 @@ public class Enemy : MonoBehaviour {
 	public int health, maxHealth;
 	public int strength;
 
-	//Animator animator;
-	NavMeshAgent agent;
-	bool isDead = false;
-
-	private void Awake() {
-		//animator = GetComponent<Animator>();
-		agent = GetComponent<NavMeshAgent>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (isDead) return;
-        if (health <= 0) {
-			isDead = true;
-			//animator.SetTrigger("dead");
-			agent.enabled = false;
-			Destroy(gameObject, 5f);
-		}
-    }
+	protected Animator animator;
+	protected NavMeshAgent agent;
+	protected bool isDead = false;
 
 	public void takeDamage(int damage) {
 		if (isDead) return;
 		health -= damage;
 		if (health <= 0) health = 0;
-	}
-
-	private void OnTriggerEnter(Collider other) {
-		
-		if (other.CompareTag("Player")) {
-
-			Debug.Log("Enemy trigger with player");
-            other.gameObject.GetComponent<HealthPoints>().removeHealth(strength);
-		}
-	}
-
-	private void OnCollisionEnter(Collision collision) {
-		
-		if (collision.collider.CompareTag("Player")) {
-
-			Debug.Log("Enemy collision with player");
-            collision.gameObject.GetComponent<HealthPoints>().removeHealth(strength);
-		}
 	}
 }
