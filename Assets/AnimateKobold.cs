62ad917c5ca89fe09d58306d0ea4b89e84d1b8d6
GustyWinds24 <tgustis@hotmail.com>
2018-11-03 20:02:05 -0500

    Added new enemy to tutorial level.
    Added new enemy to tutorial level.

---
+++ Assets/AnimateKobold.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimateKobold : Enemy {
+
+    // current position of kolbold
+    float currentPositionX;
+    float currentPositionZ;
+    float startPositionX;
+    float startPositionZ;
+
+    // for the player when he steps into the navmesh
+    ulong count = 0;
+    GameObject player;
+    Transform playerTransform;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = player.transform;
+        agent = GetComponent<NavMeshAgent>();
+        animator = gameObject.GetComponent<Animator>();
+        currentPositionX = transform.position.x;
+        currentPositionZ = transform.position.z;
+        startPositionX = currentPositionX;
+        startPositionZ = currentPositionZ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPositionX = transform.position.x;
+        currentPositionZ = transform.position.z;
+        if (currentPositionX != startPositionX && currentPositionZ != startPositionZ)
+        {
+            animator.SetBool("run", true);
+        }
+        if (isDead != true)
+        {
+            if (health <= 0)
+            {
+                isDead = true;
+                animator.SetTrigger("dead");
+                agent.enabled = false;
+                Destroy(gameObject, 5f);
+            }
+        }
+        if (count % 100 == 0)
+        {
+            agent.SetDestination(playerTransform.position);
+            this.
+            count = 0;
+        }
+        count++;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+
+            Debug.Log("Enemy trigger with player");
+            animator.SetTrigger("attack");
+            other.gameObject.GetComponent<HealthPoints>().removeHealth(strength);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.collider.CompareTag("Player"))
+        {
+
+            Debug.Log("Enemy collision with player");
+            animator.SetTrigger("attack");
+            collision.gameObject.GetComponent<HealthPoints>().removeHealth(strength);
+        }
+    }
+}
