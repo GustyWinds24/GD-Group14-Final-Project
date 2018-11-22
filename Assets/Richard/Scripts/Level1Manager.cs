513bd831de4c5ebdf51c47b57629d75ce2925c65
kbgagt <kbgagt@gmail.com>
2018-11-21 20:40:35 -0600

    Update Level1Manager.cs

--- Assets/Richard/Scripts/Level1Manager.cs
+++ Assets/Richard/Scripts/Level1Manager.cs
@@ -15,7 +15,7 @@ public class Level1Manager : MonoBehaviour {
     public static Level1Manager instance;
 
     bool hasKey1 = false, hasKey2 = false, hasKey3 = false; 
-	short intel = 0;
+	short intel = 2;
 	//bool hasBigKey = false;
 
 	AudioSource soundEffects;
@@ -153,7 +153,7 @@ public class Level1Manager : MonoBehaviour {
 	}
 	
 	public void levelComplete() {
-		GameManager.instance.goHome();
+		GameManager.instance.loadLevel((GameManager.instance.getCurrentLevel() + 1));
 	}
 
 	public void onClickPrompt () {
