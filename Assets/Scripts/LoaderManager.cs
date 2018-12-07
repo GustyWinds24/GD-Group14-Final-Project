6d41fc2c4c3575ca3d1a5eb9563b2804ca05edb6
GustyWinds24 <tgustis@hotmail.com>
2018-12-07 14:42:49 -0600

    Added a working loading menu and credits panel.
    Added a working loading menu and credits panel.

---
+++ Assets/Scripts/LoaderManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoaderManager : MonoBehaviour {
+
+    public GameObject MainPanel;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Level0Loader()
+    {
+        MainPanel.SetActive(false);
+        SceneManager.LoadScene("Level0");
+    }
+
+    public void Level1Loader()
+    {
+        MainPanel.SetActive(false);
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Level2Loader()
+    {
+        MainPanel.SetActive(false);
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void Level3Loader()
+    {
+        MainPanel.SetActive(false);
+        SceneManager.LoadScene("Level3");
+    }
+}
