d9b4ec4f047c71f6998d77d3b2dfb7165b309005
GustyWinds24 <tgustis@hotmail.com>
2018-12-09 17:04:57 -0600

    Completed Level 3.
    Completed Level 3.

---
+++ Assets/WinTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTrigger : MonoBehaviour {
+
+    public AudioSource winAudio;
+    public bool artifactIsGot;
+    private AudioSource cameraAudio;
+    private AudioSource triggerAudio;
+
+    // Use this for initialization
+    void Start () {
+        winAudio = GetComponent<AudioSource>();
+        cameraAudio = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
+        triggerAudio = GameObject.FindGameObjectWithTag("Box1").GetComponent<AudioSource>();
+        artifactIsGot = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && artifactIsGot == true)
+        {
+            cameraAudio.loop = false;
+            cameraAudio.Stop();
+            triggerAudio.loop = false;
+            triggerAudio.Stop();
+            winAudio.Play();
+            Level3Manager.instance.wonGame();
+        }
+    }
+}
